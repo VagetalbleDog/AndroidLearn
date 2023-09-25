56d89f75f58a12653feec7ac78906b623754557c
wfzhu6 <wfzhu6@iflytek.com>
2023-09-25 11:32:00 +0800

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("abcdefg");
+        setContentView(R.layout.activity_main);
+    }
+}
