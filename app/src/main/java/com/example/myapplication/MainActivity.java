8fafdbcecf20ba797f57e9f9e979f55d51cc8f09
wfzhu6 <wfzhu6@iflytek.com>
2023-09-27 17:41:10 +0800

    feat: new commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.myapplication;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("abcdefg");
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e(TAG,"onClick");
+            }
+        });
+        btn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Log.e(TAG,"onCLongClick");
+                return false;
+            }
+        });
+        ProgressBar progressBar = findViewById(R.id.pb);
+        progressBar.setProgress(30);
+        progressBar.setMax(100);
+        progressBar.setIndeterminate(true);
     }
 }
